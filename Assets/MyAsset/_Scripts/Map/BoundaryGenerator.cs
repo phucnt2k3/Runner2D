6dfa8d7881119ffdc1547c548ee90f5d6403c3b3
phucnt2k3 <phucnt@gameestudio.com>
2023-04-27 15:13:29 +0700

    update: boundary generator with room config

--- Assets/MyAsset/_Scripts/Map/BoundaryGenerator.cs
+++ Assets/MyAsset/_Scripts/Map/BoundaryGenerator.cs
@@ -2,30 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoundaryGenerator : MonoBehaviour
+public class BoundaryGenerator : MonoBehaviour 
 {
+    [SerializeField] private RoomInfo _roomInfo;
     public GameObject boundaryPoint;
-
-    // Start is called before the first frame update
-    void Start() { }
-
-    // Update is called once per frame
-    void Update() { }
 
     public void Generate(int row, int col, string parentDir)
     {
-        for (int i = 0; i < row * 8; ++i)
+        for (int i = 0; i < row * _roomInfo.rows; ++i)
         {
             GenerateAndSetParent(-0.5f, 0.5f + i, parentDir);
 
-            GenerateAndSetParent(-0.5f + col * 12 + 1, 0.5f + i, parentDir);
+            GenerateAndSetParent(-0.5f + col * _roomInfo.cols + 1, 0.5f + i, parentDir);
         }
 
-        for (int i = 0; i < col * 12 + 2; ++i)
+        for (int i = 0; i < col * _roomInfo.cols + 2; ++i)
         {
             GenerateAndSetParent(-0.5f + i, -0.5f, parentDir);
 
-            GenerateAndSetParent(-0.5f + i, -0.5f + row * 8 + 1, parentDir);
+            GenerateAndSetParent(-0.5f + i, -0.5f + row * _roomInfo.rows + 1, parentDir);
         }
     }
 
